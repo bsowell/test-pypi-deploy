f7a940f36b07a15a13c3b79acc799f9892e84ca7
Ben Sowell <ben@aryn.ai>
2023-09-18 16:24:11 -0700

    Fix release drafter and add new test command.

--- test_deploy/test.py
+++ test_deploy/test.py
@@ -7,6 +7,13 @@ def cli():
 
 
 @cli.command()
+@click.option("--left", type=int)
+@click.option("--right", type=int)
+def sum(left, right):
+    print(f"{left + right}")
+
+
+@cli.command()
 @click.option("--text", type=str)
 def echo(text):
     print(text)
